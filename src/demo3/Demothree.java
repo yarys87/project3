21d9ce5aa3218840810399bb04a126f18bf5ad3a
yarys87 <94873403+yarys87@users.noreply.github.com>
2021-11-24 20:45:18 -0500

    added println line

--- src/demo3/Demothree.java
+++ src/demo3/Demothree.java
@@ -4,7 +4,7 @@ public class Demothree {
 public static void main(String[] args) {
 	System.out.println("stressfull day");
 	System.out.println("we cannot quit studying now");
-
+        System.out.println("one more try");
 	}
 // comment about pull function, add a comment
 		public static void addnum(int a, int b) {
