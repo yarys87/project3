0eaf0c4479ebe89e041be85925a44f14d099e933
yarys87 <94873403+yarys87@users.noreply.github.com>
2021-11-24 20:42:34 -0500

    Update Demothree.java

--- src/demo3/Demothree.java
+++ src/demo3/Demothree.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	System.out.println("we cannot quit studying now");
 
 	}
-
+// comment about pull function
 		public static void addnum(int a, int b) {
 	}
 }
