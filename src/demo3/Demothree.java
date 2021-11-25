9680978f13f64b1410febadb4116899056b70ef7
yary <yary@matts-mbp.lan>
2021-11-24 20:06:46 -0500

    commit added method

--- src/demo3/Demothree.java
+++ src/demo3/Demothree.java
@@ -4,5 +4,10 @@ public class Demothree {
 public static void main(String[] args) {
 	System.out.println("stressfull day");
 	System.out.println("we cannot quit studying now");
+
+	}
+
+		public static void addnum(int a, int b) {
+	}
 }
-}
+
